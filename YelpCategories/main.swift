05f99576c34ce605c2bf9d327fe09a08d2b1e30b
Michael Comella <michael.l.comella@gmail.com>
2017-02-21 15:19:53 -0800

    Initial Commit

---
+++ YelpCategories/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YelpCategories
+//
+//  Created by Michael Comella on 2/21/17.
+//  Copyright © 2017 Michael Comella. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
